ba2ecec6387278def4af5e848c806fef5f850954
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2022-09-26 00:58:10 -0500

    Create script to add exercise links.

--- questions/add_exercise_links.py
+++ questions/add_exercise_links.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python
 
-from pathlib import Path
+for num in range(11, 22):
+    text = f"""
+## Project Link
 
-root_path = Path(".")
-
-for path in root_path.glob("**/exercise*.py"):
-    print(path)
-
+For a link to the running project, see: [exercise_{num}](https://projects.pty.cwhq-apps.com/?filename=/code-challenge-2022/exercise_{num}/main.py)
+    """
+    with open(f"question-{num}.md", mode="at", encoding="utf-8") as file:
+        file.write(text)
